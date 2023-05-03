9c73505b99a92a21036dc488f52ec48b25935348
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-03 00:12:43 -0500

    setNav func added to get an object of nav items

--- utils/index.js
+++ utils/index.js
@@ -7,4 +7,21 @@ function setMainView(view){
     }
 }
 
-module.exports = { setMainView };
+function setNavs(currentHref, navs, isAuthenticated) {
+    const _navs = navs.map(nav => {
+        nav.className = "";
+        if(nav.href === currentHref){
+            nav.className = "active"
+        }
+        return nav
+    }).filter(nav => {
+        if(!isAuthenticated) {
+            return !nav.isPrivate
+        } else {
+            return nav.isPrivate || nav.isPrivate === undefined
+        }
+    })
+    return {navs: _navs}
+}
+
+module.exports = { setMainView, setNavs };
