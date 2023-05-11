2f0a28c4ee519de4bf1ea4198fd14e806b7f602f
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-11 00:49:11 -0500

    sing-up misspelling fixed

--- dist/main.js
+++ dist/main.js
@@ -120,7 +120,7 @@ if(window.location.pathname === "/login"){
         <input type="password" name="password" class="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"/>
       </label>
       
-      <input type="submit" value="Sign in" class=" mt-4 py-2 text-white rounded-md cursor-pointer bg-indigo-600 hover:bg-indigo-500 active:bg-violet-700 hover:outline-none  sm:text-sm"/>
+      <input type="submit" value="Sign up" class=" mt-4 py-2 text-white rounded-md cursor-pointer bg-indigo-600 hover:bg-indigo-500 active:bg-violet-700 hover:outline-none  sm:text-sm"/>
 
       <a class="text-blue-600 pt-4 text-center sm:text-sm" href="/login">Already have an account?</a>
       `;
