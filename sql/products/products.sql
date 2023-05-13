2d1a1412a49dafb08dd592f0a5352cb259224dba
Henryesc <122941763+Henryesc@users.noreply.github.com>
2023-05-12 20:53:01 -0500

    Singleproduct (#21)
    * adds styling to single product
    
    * adds css to single products page
    
    * adds descriptions to products in database
    
    * changed photos that were linked to the wrong products
    
    * corrected one last photo

--- sql/products/products.sql
+++ sql/products/products.sql
@@ -10,53 +10,53 @@ CREATE TABLE stock (
   category VARCHAR(255) NOT NULL,
   image_url_one VARCHAR(300),
   image_url_two VARCHAR(300),
-  image_url_three VARCHAR(300),
+  description VARCHAR(200),
   PRIMARY KEY (id)
 );
 
 
-INSERT INTO stock (id, name, priceincents, category, favorites, image_url_one, image_url_two)
+INSERT INTO stock (id, name, priceincents, category, image_url_one, image_url_two, description)
 VALUES
-(1, 'Zenith', 74500, 'smartphone', 'backendphotos/Zenith.png', 'backendphotos/ZenithTime2.png'),
-(2, 'NovaTech', 98750, 'smartphone', 'backendphotos/NovaTech.png', 'backendphotos/Nexus2.png'),
-(3, 'Hyperion', 61400, 'smartphone', 'backendphotos/Hyperion.png', 'backendphotos/Hyperion.png'),
-(4, 'Phoenix', 81600, 'smartphone', 'backendphotos/Phoenix.png', 'backendphotos/Phoenix2.png'),
-(5, 'Vortex', 59100, 'smartphone', 'backendphotos/Vortex.png', 'backendphotos/Vortex2.png'),
-(6, 'Aurora', 70400, 'smartphone', 'backendphotos/Aurora.png', 'backendphotos/Aurora2.png'),
-(7, 'ApexTech', 90500, 'smartphone', 'backendphotos/ApexTech.png', 'backendphotos/ApexTech2.png'),
-(8, 'Nebula', 62300, 'smartphone', 'backendphotos/Nebula.png', 'backendphotos/Nexus2.png'),
-(9, 'Velocity', 75000, 'laptop', 'backendphotos/Velocity.png', 'backendphotos/Velocity2.png'),
-(10, 'Quantum', 82000, 'laptop', 'backendphotos/Quantum.png', 'backendphotos/Quantum2.png'),
-(11, 'Hypernova', 96000, 'laptop', 'backendphotos/Hypernova.png', 'backendphotos/Hypernova2.png'),
-(12, 'Thunderbolt', 52000, 'laptop', 'backendphotos/Thunderbolt.png', 'backendphotos/Thunderbolt2.png'),
-(13, 'Fusion', 64000, 'laptop', 'backendphotos/Fusion.png', 'backendphotos/Fusion2.png'),
-(14, 'Infinity', 78000, 'laptop', 'backendphotos/Infinity.png', 'backendphotos/Infinity2.png'),
-(15, 'TitanTech', 88000, 'laptop', 'backendphotos/TitanTech.png', 'backendphotos/TitanTech2.png'),
-(16, 'Blaze', 59000, 'laptop', 'backendphotos/Blaze.png', 'backendphotos/Blaze2.png'),
-(17, 'PixelPad', 60000, 'tablet', 'backendphotos/PixelPad.png', 'backendphotos/PixelPad2.png'),
-(18, 'MatrixTab', 70000, 'tablet', 'backendphotos/MatrixTab.png', 'backendphotos/MatrixTab2.png'),
-(19, 'Neoteric', 80000, 'tablet', 'backendphotos/Neoteric.png', 'backendphotos/Neoteric2.png'),
-(20, 'VelocityTab', 90000, 'tablet', 'backendphotos/VelocityTab.png', 'backendphotos/VelocityTab2.png'),
-(21, 'Nexus', 65000, 'tablet', 'backendphotos/Nexus.png', 'backendphotos/Nexus2.png'),
-(22, 'SkyPad', 75000, 'tablet', 'backendphotos/SkyPad.png', 'backendphotos/SkyPad2.png'),
-(23, 'PhoenixTab', 55000, 'tablet', 'backendphotos/PhoenixTab.png', 'backendphotos/PhoenixTab2.png'),
-(24, 'Stellar', 85000, 'tablet', 'backendphotos/Stellar.png', 'backendphotos/Stellar2.png'),
-(25, 'TimeShift', 20000, 'smartwatch', 'backendphotos/TimeShift.png', 'backendphotos/TimeShift2.png'),
-(26, 'HyperionWatch', 25000, 'smartwatch', 'backendphotos/HyperionWatch.png', 'backendphotos/HyperionWatch2.png'),
-(27, 'ZenithTime', 15000, 'smartwatch', 'backendphotos/ZenithTime.png', 'backendphotos/ZenithTime2.png'),
-(28, 'NovaWatch', 30000, 'smartwatch', 'backendphotos/NovaWatch.png', 'backendphotos/NovaWatch2.png'),
-(29, 'SkyTime', 12000, 'smartwatch', 'backendphotos/SkyTime.png', 'backendphotos/SkyTime2.png'),
-(30, 'FusionWatch', 18000, 'smartwatch', 'backendphotos/FusionWatch.png', 'backendphotos/FusionWatch2.png'),
-(31, 'ApexTime', 22000, 'smartwatch', 'backendphotos/ApexTime.png', 'backendphotos/ApexTime2.png'),
-(32, 'VortexWatch', 28000, 'smartwatch', 'backendphotos/VortexWatch.png', 'backendphotos/VortexWatch2.png'),
-(33, 'Ascendia', 28000, 'keyboards', 'backendphotos/Ascendia.png', 'backendphotos/Ascendia2.png'),
-(34, 'Veritas', 22500, 'keyboards', 'backendphotos/Veritas.png', 'backendphotos/Veritas2.png'),
-(35, 'Luminate', 17000, 'keyboards', 'backendphotos/Luminate.png', 'backendphotos/Luminate2.png'),
-(36, 'Elysium', 15000, 'keyboards', 'backendphotos/Elysium.png', 'backendphotos/Elysium2.png'),
-(37, 'Aetherius', 26500, 'keyboards', 'backendphotos/Aetherius.png', 'backendphotos/Aetherius2.png'),
-(38, 'Valtara', 24000, 'keyboards', 'backendphotos/Valtara.png', 'backendphotos/Valtara2.png'),
-(39, 'Celestia', 22000, 'keyboards', 'backendphotos/Celestia.png', 'backendphotos/Celestia2.png'),
-(40, 'Solace', 11000, 'keyboards', 'backendphotos/Solace.png', 'backendphotos/Solace2.png');
+(1, 'Zenith', 74500, 'smartphone', 'backendphotos/Zenith.png', 'backendphotos/Zenith2.png', 'Experience the pinnacle of technology with the Zenith smartphones. With a sleek design and advanced features, it''s the phone of the future.'),
+(2, 'NovaTech', 98750, 'smartphone', 'backendphotos/NovaTech.png', 'backendphotos/NovaTech2.png', 'Unleash the power of technology with the NovaTech smartphone. With a fast processor and stunning display, it''s the perfect device for tech enthusiasts.'),
+(3, 'Hyperion', 61400, 'smartphone', 'backendphotos/Hyperion.png', 'backendphotos/Hyperion2.png', 'Get ready to be impressed with the Hyperion smartphone. With a high-quality camera and impressive performance, it''s perfect for content creators.'),
+(4, 'Phoenix', 81600, 'smartphone', 'backendphotos/Phoenix.png', 'backendphotos/Phoenix2.png', 'Rise from the ashes with the Phoenix smartphone. With a long-lasting battery and advanced features, it''s the perfect device for on-the-go lifestyles.'),
+(5, 'Vortex', 59100, 'smartphone', 'backendphotos/Vortex.png', 'backendphotos/Vortex2.png', 'Get swept away by the Vortex smartphone. With a powerful processor and cutting-edge features, it''s the perfect device for multitaskers.'),
+(6, 'Aurora', 70400, 'smartphone', 'backendphotos/Aurora.png', 'backendphotos/Aurora2.png', 'Illuminate your life with the Aurora smartphone. With a stunning display and advanced camera, it''s perfect for capturing and sharing life''s moments.'),
+(7, 'ApexTech', 90500, 'smartphone', 'backendphotos/ApexTech.png', 'backendphotos/ApexTech2.png', 'Reach the peak of performance with the ApexTech smartphone. With a lightning-fast processor and impressive features, it''s perfect for power users.'),
+(8, 'Nebula', 62300, 'smartphone', 'backendphotos/Nebula.png', 'backendphotos/Nebula2.png', 'Enter a world of possibilities with the Nebula smartphone. With a sleek design and advanced features, it''s perfect for people on the go.'),
+(9, 'Velocity', 75000, 'laptop', 'backendphotos/Velocity.png', 'backendphotos/Velocity2.png', 'Take your work to new heights with the Velocity laptop. With lightning-fast performance and a sleek design, it''s the perfect device for professionals.'),
+(10, 'Quantum', 82000, 'laptop', 'backendphotos/Quantum.png', 'backendphotos/Quantum2.png', 'Experience the power of the Quantum laptop. With advanced features and a stunning display, it''s the perfect device for gamers and creatives.'),
+(11, 'Hypernova', 96000, 'laptop', 'backendphotos/Hypernova.png', 'backendphotos/Hypernova2.png', 'Unleash your creativity with the Hypernova laptop. With a high-quality display and advanced features, it''s perfect for artists and designers.'),
+(12, 'Thunderbolt', 52000, 'laptop', 'backendphotos/Thunderbolt.png', 'backendphotos/Thunderbolt2.png', 'Experience lightning-fast performance with the Thunderbolt laptop. With a sleek design and advanced features, it''s the perfect device for multitaskers.'),
+(13, 'Fusion', 64000, 'laptop', 'backendphotos/Fusion.png', 'backendphotos/Fusion2.png', 'Experience the perfect fusion of performance and style with the Fusion laptop. With advanced features and a stunning design, it''s perfect for anyone.'),
+(14, 'Infinity', 78000, 'laptop', 'backendphotos/Infinity.png', 'backendphotos/Infinity2.png', 'Expand your horizons with the Infinity laptop. With a sleek design and advanced features, it''s the perfect device for people on the go.'),
+(15, 'TitanTech', 88000, 'laptop', 'backendphotos/TitanTech.png', 'backendphotos/TitanTech2.png', 'Conquer any task with the TitanTech laptop. With a powerful processor and advanced features, it''s the perfect device for power users.'),
+(16, 'Blaze', 59000, 'laptop', 'backendphotos/Blaze.png', 'backendphotos/Blaze2.png', 'Ignite your productivity with the Blaze laptop. With lightning-fast performance and a sleek design, it''s perfect for professionals on the go.'),
+(17, 'PixelPad', 60000, 'tablet', 'backendphotos/PixelPad.png', 'backendphotos/PixelPad2.png', 'Discover a new world of possibilities with the PixelPad tablet. With a high-quality display and advanced features, it''s perfect for creative professionals.'),
+(18, 'MatrixTab', 70000, 'tablet', 'backendphotos/MatrixTab.png', 'backendphotos/MatrixTab2.png', 'Experience the power of the MatrixTab tablet. With advanced features and a sleek design, it''s perfect for anyone on the go.'),
+(19, 'Neoteric', 80000, 'tablet', 'backendphotos/Neoteric.png', 'backendphotos/Neoteric2.png', 'Explore new horizons with the Neoteric tablet. With a stunning display and advanced features, it''s perfect for people on the go.'),
+(20, 'VelocityTab', 90000, 'tablet', 'backendphotos/VelocityTab.png', 'backendphotos/VelocityTab2.png', 'Take your productivity to new heights with the VelocityTab tablet. With advanced features and lightning-fast performance, it''s perfect for professionals.'),
+(21, 'Nexus', 65000, 'tablet', 'backendphotos/Nexus.png', 'backendphotos/Nexus2.png', 'Enter a new dimension with the Nexus tablet. With a sleek design and advanced features, it''s perfect for anyone who wants to stay connected.'),
+(22, 'SkyPad', 75000, 'tablet', 'backendphotos/SkyPad.png', 'backendphotos/SkyPad2.png', 'Reach for the sky with the SkyPad tablet. With a high-quality display and advanced features, it''s perfect for creative professionals on the go.'),
+(23, 'PhoenixTab', 55000, 'tablet', 'backendphotos/PhoenixTab.png', 'backendphotos/PhoenixTab2.png', 'Rise from the ashes with the PhoenixTab tablet. With a long-lasting battery and advanced features, it''s perfect for anyone on the go.'),
+(24, 'Stellar', 85000, 'tablet', 'backendphotos/Stellar.png', 'backendphotos/Stellar2.png', 'Experience stellar performance and style with the Stellar tablet. With advanced features and a stunning design, it''s perfect for anyone.'),
+(25, 'TimeShift', 20000, 'smartwatch', 'backendphotos/TimeShift.png', 'backendphotos/TimeShift2.png', 'Keep up with your busy life with TimeShift. Stylish and convenient features.'),
+(26, 'HyperionWatch', 25000, 'smartwatch', 'backendphotos/HyperionWatch.png', 'backendphotos/HyperionWatch2.png', 'Experience next-level smartwatch technology with HyperionWatch.'),
+(27, 'ZenithTime', 15000, 'smartwatch', 'backendphotos/ZenithTime.png', 'backendphotos/ZenithTime2.png', 'Elevate your style with ZenithTime. Sleek and functional design.'),
+(28, 'NovaWatch', 30000, 'smartwatch', 'backendphotos/NovaWatch.png', 'backendphotos/NovaWatch2.png', 'Your perfect workout partner. NovaWatch tracks your fitness goals and more.'),
+(29, 'SkyTime', 12000, 'smartwatch', 'backendphotos/SkyTime.png', 'backendphotos/SkyTime2.png', 'The perfect mix of style and functionality. Stay on top of your schedule with SkyTime.'),
+(30, 'FusionWatch', 18000, 'smartwatch', 'backendphotos/FusionWatch.png', 'backendphotos/FusionWatch2.png', 'Stay connected and stylish with FusionWatch. A must-have for modern living.'),
+(31, 'ApexTime', 22000, 'smartwatch', 'backendphotos/ApexTime.png', 'backendphotos/ApexTime2.png', 'Your personal assistant on your wrist. ApexTime helps you stay organized.'),
+(32, 'VortexWatch', 28000, 'smartwatch', 'backendphotos/VortexWatch.png', 'backendphotos/VortexWatch2.png', 'Take control of your life with VortexWatch. A smartwatch for power users.'),
+(33, 'Ascendia', 28000, 'keyboards', 'backendphotos/Ascendia.png', 'backendphotos/Ascendia2.png', 'Your perfect typing companion. Ascendia delivers comfort and speed.'),
+(34, 'Veritas', 22500, 'keyboards', 'backendphotos/Veritas.png', 'backendphotos/Veritas2.png', 'The ultimate keyboard for productivity. Experience the power of Veritas.'),
+(35, 'Luminate', 17000, 'keyboards', 'backendphotos/Luminate.png', 'backendphotos/Luminate2.png', 'Illuminate your workspace with Luminate. A keyboard that delivers style and performance.'),
+(36, 'Elysium', 15000, 'keyboards', 'backendphotos/Elysium.png', 'backendphotos/Elysium2.png', 'Unlock your productivity potential with Elysium. Designed for peak performance.'),
+(37, 'Aetherius', 26500, 'keyboards', 'backendphotos/Aetherius.png', 'backendphotos/Aetherius2.png', 'A keyboard that adapts to your style. Aetherius is as unique as you are.'),
+(38, 'Valtara', 24000, 'keyboards', 'backendphotos/Valtara.png', 'backendphotos/Valtara2.png', 'Elevate your typing experience with Valtara. The keyboard for professionals.'),
+(39, 'Celestia', 22000, 'keyboards', 'backendphotos/Celestia.png', 'backendphotos/Celestia2.png', 'The ultimate keyboard for gamers. Celestia delivers speed and precision.'),
+(40, 'Solace', 11000, 'keyboards', 'backendphotos/Solace.png', 'backendphotos/Solace2.png', 'The perfect keyboard for a quiet workspace. Solace delivers comfort and silence.');
 -- (33, 'SonicBloom', 10000, 'headphones'),
 -- (34, 'EchoSound', 7000, 'headphones'),
 -- (35, 'AcousticSoul', 9000, 'headphones'),
@@ -186,7 +186,7 @@ CREATE TABLE visitors (
 );
 
 
-CREATE TABLE customer (
+CREATE TABLE users (
   id INT PRIMARY KEY,
   email TEXT,
   first_name TEXT,
@@ -222,5 +222,3 @@ SELECT * FROM favorites WHERE user_id = 1;
 SELECT * FROM favorites WHERE user_id = 2;
 
 -- psql -U postgres -f ./sql/setup.sql
-
-
